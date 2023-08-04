7488d474a44cef401beebf7de51b3a2bb71abdd9
Nick <nikolay.chobanov@x3me.net>
2023-08-04 12:24:41 +0300

    remove local venv to use netbox venv

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='geo-map',
-    version='0.1',
-    description='Geological Map with site locations',
+    name="geo_map",
+    version="0.1",
+    description="Geological Map with site locations",
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
