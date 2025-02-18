1b1d234e1b7fd8e61a8cdece15b097364dce5862
Eva Martinova <evelina.martinova@x3me.net>
2025-02-18 11:00:45 +0200

    [bump] version to 0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geo_map",
-    version="0.4.2",
+    version="0.4.3",
     description="Geological Map with site locations",
     install_requires=[],
     packages=find_packages(),
